ff06bb9c4bdf41398b14fa9aa9f6a7448f98de10
Hamish Peebles <hamishpeebles@gmail.com>
2018-09-28 13:53:53 +0100

    Extension methods to add Redis and Metrics to default config

---
+++ src/CacheMeIfYouCan.Prometheus/DefaultCacheConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CacheMeIfYouCan.Configuration;
+
+namespace CacheMeIfYouCan.Prometheus
+{
+    public static class DefaultCacheConfigurationExtensions
+    {
+        public static DefaultCacheConfiguration WithMetrics(this DefaultCacheConfiguration config)
+        {
+            config.OnResult = MetricsTracker.OnResult;
+            config.OnFetch = MetricsTracker.OnFetch;
+            
+            return config;
+        }
+    }
+}
---
+++ src/CacheMeIfYouCan.Redis/DefaultCacheConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CacheMeIfYouCan.Configuration;
+
+namespace CacheMeIfYouCan.Redis
+{
+    public static class DefaultCacheConfigurationExtensions
+    {
+        public static DefaultCacheConfiguration WithRedis(this DefaultCacheConfiguration config, Action<RedisCacheFactoryConfig> configAction)
+        {
+            var redisConfig = new RedisCacheFactoryConfig();
+
+            configAction(redisConfig);
+        
+            config.RemoteCacheFactory = new RedisCacheFactory(redisConfig);
+        
+            return config;
+        }
+    }
+}
