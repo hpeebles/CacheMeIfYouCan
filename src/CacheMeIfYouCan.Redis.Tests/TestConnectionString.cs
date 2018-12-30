0d90cf9860c2a9e24e94a905594bc23c12be0f99
Hamish Peebles <hamishpeebles@gmail.com>
2018-12-30 14:01:10 +0000

    Get Redis connection details from environment variables

--- src/CacheMeIfYouCan.Redis.Tests/TestConnectionString.cs
+++ src/CacheMeIfYouCan.Redis.Tests/TestConnectionString.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace CacheMeIfYouCan.Redis.Tests
 {
     public static class TestConnectionString
     {
-        public static readonly string Value = $"redis-test:{Port},password=xxxxxxxx";
+        public static readonly string Value = $"redis-test:{Port},password={Password}";
 
-        private const int Port = 14456;
+        private static string Password => Environment.GetEnvironmentVariable("RedisTestPassword");
+        private static int Port => Int32.Parse(Environment.GetEnvironmentVariable("RedisTestPort"));
     }
 }
