7413f9b550ae6375cd5ae9ad35fa64921c460207
Hamish Peebles <hamishpeebles@gmail.com>
2019-02-17 23:56:14 +0000

    Add Benchmarks.CacheComparison project

---
+++ src/Benchmarks/Benchmarks.CacheComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Benchmarks.CacheComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomisableBenchmarksRunner.Run();
+        }
+    }
+}
