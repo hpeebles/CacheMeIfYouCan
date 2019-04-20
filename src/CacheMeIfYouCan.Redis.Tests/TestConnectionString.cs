3f1fe285b1d1da201cfada3072ec6af835189fdd
Hamish Peebles <hamishpeebles@gmail.com>
2019-04-21 00:20:28 +0100

    Fix Redis test connection issue

--- src/CacheMeIfYouCan.Redis.Tests/TestConnectionString.cs
+++ src/CacheMeIfYouCan.Redis.Tests/TestConnectionString.cs
@@ -4,9 +4,9 @@ namespace CacheMeIfYouCan.Redis.Tests
 {
     public static class TestConnectionString
     {
-        public static readonly string Value = $"redis-test:{Port},password={Password}";
+        public static readonly string Value = $"{Endpoint},password={Password}";
 
+        private static string Endpoint => Environment.GetEnvironmentVariable("RedisTestEndpoint");
         private static string Password => Environment.GetEnvironmentVariable("RedisTestPassword");
-        private static int Port => Int32.Parse(Environment.GetEnvironmentVariable("RedisTestPort"));
     }
 }
