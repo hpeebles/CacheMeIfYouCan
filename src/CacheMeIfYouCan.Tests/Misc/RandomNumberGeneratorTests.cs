3a8cd85f71eff889f9ba9dbef6d4163ad4650de9
Hamish Peebles <hamishpeebles@gmail.com>
2019-10-23 14:42:57 +0100

    Make test more reliable

--- src/CacheMeIfYouCan.Tests/Misc/RandomNumberGeneratorTests.cs
+++ src/CacheMeIfYouCan.Tests/Misc/RandomNumberGeneratorTests.cs
@@ -13,14 +13,14 @@ namespace CacheMeIfYouCan.Tests.Misc
             var rng = new RandomNumberGenerator(-100, 100);
 
             var numbers = Enumerable
-                .Range(0, 100)
+                .Range(0, 1000)
                 .Select(i => rng.GetNext())
                 .OrderBy(i => i)
                 .ToArray();
             
             numbers.First().Should().BeGreaterThan(-100).And.BeLessThan(-80);
 
-            numbers[50].Should().BeGreaterThan(-30).And.BeLessThan(30);
+            numbers[500].Should().BeGreaterThan(-30).And.BeLessThan(30);
 
             numbers.Last().Should().BeGreaterThan(80).And.BeLessThan(100);
         }
