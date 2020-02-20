f2b2118b6bd87eed42a82162d3d44fdc25001be8
Hamish Peebles <hamishpeebles@gmail.com>
2020-02-20 23:54:24 +0000

    Updated benchmarks

--- benchmarks/CacheMeIfYouCan.Benchmarks/CachedFunctionWithEnumerableKeys.cs
+++ benchmarks/CacheMeIfYouCan.Benchmarks/CachedFunctionWithEnumerableKeys.cs
@@ -20,9 +20,22 @@ namespace CacheMeIfYouCan.Benchmarks
         private readonly List<int> _oneHundredHits = Enumerable.Range(0, 100).ToList();
         private readonly List<int> _oneHundredMisses = Enumerable.Range(100, 100).ToList();
         private readonly List<int> _oneHundredHitsAndOneHundredMisses = Enumerable.Range(0, 200).ToList();
+        private readonly Task<List<KeyValuePair<int, int>>> _oneHitResults;
+        private readonly Task<List<KeyValuePair<int, int>>> _oneMissResults;
+        private readonly Task<List<KeyValuePair<int, int>>> _oneHitAndOneMissResults;
+        private readonly Task<List<KeyValuePair<int, int>>> _oneHundredHitsResults;
+        private readonly Task<List<KeyValuePair<int, int>>> _oneHundredMissesResults;
+        private readonly Task<List<KeyValuePair<int, int>>> _oneHundredHitsAndOneHundredMissesResults;
 
         public CachedFunctionWithEnumerableKeys()
         {
+            _oneHitResults = BuildResults(_oneHit);
+            _oneMissResults = BuildResults(_oneMiss);
+            _oneHitAndOneMissResults = BuildResults(_oneHitAndOneMiss);
+            _oneHundredHitsResults = BuildResults(_oneHundredHits);
+            _oneHundredMissesResults = BuildResults(_oneHundredMisses);
+            _oneHundredHitsAndOneHundredMissesResults = BuildResults(_oneHundredHitsAndOneHundredMisses);
+            
             var cache = new DictionaryCache<int, int>();
             for (var i = 0; i < 100; i++)
                 cache.Set(i, i, TimeSpan.FromHours(1));
@@ -48,8 +61,7 @@ namespace CacheMeIfYouCan.Benchmarks
 #else
             BenchmarkRunner.Run<CachedFunctionWithEnumerableKeys>(ManualConfig
                 .Create(DefaultConfig.Instance)
-                .With(MemoryDiagnoser.Default)
-                .With(Job.Default.With(new GcMode { Force = false })));
+                .With(MemoryDiagnoser.Default));
 #endif
         }
 
@@ -71,7 +83,18 @@ namespace CacheMeIfYouCan.Benchmarks
         [Benchmark]
         public Task<List<KeyValuePair<int, int>>> OneHundredHitsAndOneHundredMisses() => _func(_oneHundredHitsAndOneHundredMisses);
 
-        private static Task<List<KeyValuePair<int, int>>> OriginalFunc(List<int> list)
+        private Task<List<KeyValuePair<int, int>>> OriginalFunc(List<int> list)
+        {
+            return list.Count switch
+            {
+                1 => (list[0] == 0 ? _oneHitResults : _oneMissResults),
+                2 => _oneHitAndOneMissResults,
+                100 => (list[0] == 0 ? _oneHundredHitsResults : _oneHundredMissesResults),
+                _ => _oneHundredHitsAndOneHundredMissesResults
+            };
+        }
+        
+        private static Task<List<KeyValuePair<int, int>>> BuildResults(List<int> list)
         {
             var values = new List<KeyValuePair<int, int>>(list.Count);
             foreach (var i in list)
@@ -89,16 +112,15 @@ BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18362
 Intel Core i7-9750H CPU 2.60GHz, 1 CPU, 12 logical and 6 physical cores
 .NET Core SDK=3.1.100
   [Host]     : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
-  Job-TRYGSH : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
+  DefaultJob : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
 
-Force=False
 
-|                            Method |        Mean |       Error |      StdDev |      Median |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
-|---------------------------------- |------------:|------------:|------------:|------------:|-------:|-------:|------:|----------:|
-|                            OneHit |    405.7 ns |     8.14 ns |    21.74 ns |    396.4 ns | 0.0892 |      - |     - |     560 B |
-|                           OneMiss |    934.9 ns |    18.47 ns |    15.42 ns |    933.6 ns | 0.1240 | 0.0305 |     - |     784 B |
-|                  OneHitAndOneMiss |  1,136.1 ns |    42.72 ns |    41.96 ns |  1,120.5 ns | 0.1507 | 0.0381 |     - |     952 B |
-|                    OneHundredHits | 11,577.5 ns |    63.07 ns |    55.91 ns | 11,577.3 ns | 0.8698 |      - |     - |    5536 B |
-|                  OneHundredMisses | 45,794.6 ns | 2,881.77 ns | 8,360.53 ns | 40,161.3 ns | 2.0752 | 0.1221 |     - |   13233 B |
-| OneHundredHitsAndOneHundredMisses | 61,323.8 ns | 2,366.91 ns | 6,866.83 ns | 58,131.4 ns | 2.5024 | 0.6104 |     - |   15904 B |
+|                            Method |        Mean |     Error |      StdDev |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
+|---------------------------------- |------------:|----------:|------------:|-------:|-------:|------:|----------:|
+|                            OneHit |    412.5 ns |   8.22 ns |     8.07 ns | 0.0892 |      - |     - |     560 B |
+|                           OneMiss |    891.3 ns |  17.15 ns |    16.04 ns | 0.1030 | 0.0257 |     - |     648 B |
+|                  OneHitAndOneMiss |  1,076.7 ns |  11.49 ns |     9.60 ns | 0.1297 | 0.0324 |     - |     816 B |
+|                    OneHundredHits | 12,235.5 ns | 156.05 ns |   145.96 ns | 0.8698 |      - |     - |    5536 B |
+|                  OneHundredMisses | 37,087.0 ns | 728.67 ns | 1,134.45 ns | 1.9531 | 0.0610 |     - |   12603 B |
+| OneHundredHitsAndOneHundredMisses | 48,958.5 ns | 960.44 ns |   851.40 ns | 2.3804 | 0.9155 |     - |   14976 B |
 */
--- benchmarks/CacheMeIfYouCan.Benchmarks/CachedFunctionWithSingleKey.cs
+++ benchmarks/CacheMeIfYouCan.Benchmarks/CachedFunctionWithSingleKey.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
@@ -12,6 +13,7 @@ namespace CacheMeIfYouCan.Benchmarks
     public class CachedFunctionWithSingleKey
     {
         private readonly Func<int, Task<int>> _func;
+        private readonly Task<int>[] _results = Enumerable.Range(1, 2).Select(Task.FromResult).ToArray();
 
         public CachedFunctionWithSingleKey()
         {
@@ -35,8 +37,7 @@ namespace CacheMeIfYouCan.Benchmarks
 #else
             BenchmarkRunner.Run<CachedFunctionWithSingleKey>(ManualConfig
                 .Create(DefaultConfig.Instance)
-                .With(MemoryDiagnoser.Default)
-                .With(Job.Default.With(new GcMode { Force = false })));
+                .With(MemoryDiagnoser.Default));
 #endif
         }
 
@@ -46,7 +47,7 @@ namespace CacheMeIfYouCan.Benchmarks
         [Benchmark]
         public Task<int> CacheMiss() => _func(2);
 
-        private static Task<int> OriginalFunc(int k) => Task.FromResult(k);
+        private Task<int> OriginalFunc(int k) => _results[k - 1];
     }
 }
 
@@ -57,12 +58,11 @@ BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18362
 Intel Core i7-9750H CPU 2.60GHz, 1 CPU, 12 logical and 6 physical cores
 .NET Core SDK=3.1.100
   [Host]     : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
-  Job-GBDCCG : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
+  DefaultJob : .NET Core 3.1.0 (CoreCLR 4.700.19.56402, CoreFX 4.700.19.56404), X64 RyuJIT
 
-Force=False
 
-|    Method |     Mean |    Error |    StdDev |   Median |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
-|---------- |---------:|---------:|----------:|---------:|-------:|-------:|------:|----------:|
-|  CacheHit | 129.6 ns |  0.89 ns |   0.83 ns | 129.7 ns |      - |      - |     - |         - |
-| CacheMiss | 555.6 ns | 40.48 ns | 118.72 ns | 482.5 ns | 0.0162 | 0.0038 |     - |     104 B |
+|    Method |     Mean |    Error |   StdDev |   Median |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
+|---------- |---------:|---------:|---------:|---------:|-------:|-------:|------:|----------:|
+|  CacheHit | 131.3 ns |  0.81 ns |  0.68 ns | 131.4 ns |      - |      - |     - |         - |
+| CacheMiss | 415.7 ns | 31.35 ns | 92.45 ns | 364.3 ns | 0.0048 | 0.0014 |     - |      32 B |
 */
