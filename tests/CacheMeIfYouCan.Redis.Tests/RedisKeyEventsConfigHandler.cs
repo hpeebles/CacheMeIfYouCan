0b53ff809577302a52e2b204740652a2648149e9
Hamish Peebles <hamishpeebles@gmail.com>
2020-11-25 14:39:53 +0000

    Add method to enable keyspace events on new Redis instances which is required for some tests to pass

---
+++ tests/CacheMeIfYouCan.Redis.Tests/RedisKeyEventsConfigHandler.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using StackExchange.Redis;
+using Xunit;
+
+namespace CacheMeIfYouCan.Redis.Tests
+{
+    public class RedisKeyEventsConfigHandler
+    {
+        [Fact(Skip = "Run this on a new Redis instance to enable keyspace events")]
+        public void SetConfig()
+        {
+            using var connectionMultiplexer = ConnectionMultiplexer.Connect(TestConnectionString.Value + ",allowAdmin=true");
+
+            foreach (var endpoint in connectionMultiplexer.GetEndPoints())
+            {
+                var server = connectionMultiplexer.GetServer(endpoint);
+                server.ConfigSet("notify-keyspace-events", "A");
+                server.ConfigRewrite();
+            }
+        }
+    }
+}
